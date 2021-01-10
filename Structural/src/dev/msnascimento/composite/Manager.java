520b9e755308757bc2c4d955609c37fee7fd8de6
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-10 16:53:26 -0300

    Composite pattern initial class 04_02

---
+++ Structural/src/dev/msnascimento/composite/Manager.java
@@ -0,0 +1,15 @@
+package dev.msnascimento.composite;
+
+public class Manager {
+
+  private String name;
+
+  public Manager(String name) {
+    this.name = name;
+  }
+
+  public void payExpenses(int amount) {
+    System.out.println(name + " has been paid $" + amount);
+  }
+
+}
---
+++ Structural/src/dev/msnascimento/composite/Salesperson.java
@@ -0,0 +1,18 @@
+package dev.msnascimento.composite;
+
+public class Salesperson {
+
+  private String name;
+
+  private Manager manager;
+
+  public Salesperson(String name, Manager manager) {
+    this.name = name;
+    this.manager = manager;
+  }
+
+  public void payExpenses(int amount) {
+    System.out.println(name + " has been paid $" + amount);
+  }
+
+}
