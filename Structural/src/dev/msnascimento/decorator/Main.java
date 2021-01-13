e17631590db8fd7c72b7ce755feac58050eea55e
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-13 20:51:29 -0300

    Decorator pattern initial challenge

---
+++ Structural/src/dev/msnascimento/decorator/Main.java
@@ -0,0 +1,16 @@
+package dev.msnascimento.decorator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    order(new PizzaMargherita());
+    order(new PizzaHawaiian());
+    order(new PizzaPepperoni());
+  }
+
+  public static void order(Pizza pizza) {
+    System.out.println("You have ordered a " + pizza.getName() +
+        " pizza. The toppings are " + pizza.getToppings() + ".");
+  }
+
+}
---
+++ Structural/src/dev/msnascimento/decorator/Pizza.java
@@ -0,0 +1,10 @@
+package dev.msnascimento.decorator;
+
+import java.util.ArrayList;
+
+public interface Pizza {
+
+  ArrayList getToppings();
+  String getName();
+
+}
