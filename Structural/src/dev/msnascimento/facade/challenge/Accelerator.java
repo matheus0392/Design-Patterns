6869573fa0dea0a6730450f375f46dba8fee66c1
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-16 12:03:11 -0300

    Facade pattern challenge initial

---
+++ Structural/src/dev/msnascimento/facade/challenge/Accelerator.java
@@ -0,0 +1,13 @@
+package dev.msnascimento.facade.challenge;
+
+public class Accelerator {
+
+  public void press() {
+    System.out.println("Pressing accelerator down");
+  }
+
+  public void lift() {
+    System.out.println("Lifting accelerator up");
+  }
+
+}
