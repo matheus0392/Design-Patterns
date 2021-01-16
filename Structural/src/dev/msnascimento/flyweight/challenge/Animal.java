e5372f233e8beac5068a3f81a39c767ff954d0c4
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-16 16:47:45 -0300

    Flyweight pattern challenge initial

---
+++ Structural/src/dev/msnascimento/flyweight/challenge/Animal.java
@@ -0,0 +1,9 @@
+package dev.msnascimento.flyweight.challenge;
+
+public interface Animal {
+
+  String getAnimalType();
+  int[] getLocation();
+  void setLocation(int latitude, int longitude);
+
+}
