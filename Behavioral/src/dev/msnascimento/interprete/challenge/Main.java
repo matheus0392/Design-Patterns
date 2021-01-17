3c6ce5c47f645bd2a7efc4351d157ec22f9af760
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-17 12:23:37 -0300

    Interpreter pattern challenge initial

---
+++ Behavioral/src/dev/msnascimento/interprete/challenge/Main.java
@@ -0,0 +1,15 @@
+package dev.msnascimento.interprete.challenge;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    String context = "this is a a sentence";
+
+    // Interpret the sentence here
+
+    System.out.println(context);
+
+  }
+
+}
