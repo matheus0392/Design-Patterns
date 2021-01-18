850457a9bf5f2a48e5dc13f6f23ca1ad5ff5756a
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-18 02:47:40 -0300

    Mediator pattern initial

---
+++ Behavioral/src/dev/msnascimento/mediator/Driver.java
@@ -0,0 +1,9 @@
+package dev.msnascimento.mediator;
+
+public class Driver {
+
+  public void deliver(String item, int quantity, Customer customer) {
+    System.out.println(quantity + " " + item + " out for delivery to " + customer.getAddress());
+  }
+
+}
---
+++ Behavioral/src/dev/msnascimento/mediator/Main.java
@@ -0,0 +1,13 @@
+package dev.msnascimento.mediator;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Customer customer = new Customer("123 Sunny Street");
+    customer.buy("pens", 3);
+
+  }
+
+
+}
