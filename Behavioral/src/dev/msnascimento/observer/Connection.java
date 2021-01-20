f9e92270cf376595705efff5b857171b303f1169
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-19 21:34:09 -0300

    Observer pattern

--- Behavioral/src/dev/msnascimento/observer/Connection.java
+++ Behavioral/src/dev/msnascimento/observer/Connection.java
@@ -1,11 +1,20 @@
 package dev.msnascimento.observer;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class Connection {
 
-  private String status = "";
+	private String status = "";
+	private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-  public void setStatus(String status) {
-    this.status = status;
-  }
+	public void setStatus(String status) {
+		support.firePropertyChange("status", this.status, status);
+		this.status = status;
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		support.addPropertyChangeListener(listener);
+	}
 
 }
--- Behavioral/src/dev/msnascimento/observer/SocialMediaFeed.java
+++ Behavioral/src/dev/msnascimento/observer/SocialMediaFeed.java
@@ -1,13 +1,21 @@
 package dev.msnascimento.observer;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
-public class SocialMediaFeed {
+public class SocialMediaFeed implements PropertyChangeListener {
 
   private ArrayList<String> statuses = new ArrayList<>();
 
   public void printStatuses() {
     statuses.forEach(System.out::println);
   }
+
+@Override
+public void propertyChange(PropertyChangeEvent evt) {
+	statuses.add((String) evt.getNewValue());
+	
+}
 
 }
