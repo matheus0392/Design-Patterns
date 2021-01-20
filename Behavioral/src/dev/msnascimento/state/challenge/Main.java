cf210041342da303e7a65fe781297a41404cce3a
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-19 22:27:43 -0300

    State pattern challenge initial

---
+++ Behavioral/src/dev/msnascimento/state/challenge/Main.java
@@ -0,0 +1,14 @@
+package dev.msnascimento.state.challenge;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Fan fan = new Fan();
+    fan.turnUp();
+    fan.turnUp();
+    fan.turnDown();
+    fan.turnDown();
+    fan.turnUp();
+  }
+
+}
