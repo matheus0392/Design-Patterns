02275403326e4dd844a99810a66d783785599f7c
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-21 21:58:00 -0300

    Strategy pattern challenge initial

---
+++ Behavioral/src/dev/msnascimento/strategy/challenge/Customer.java
@@ -0,0 +1,14 @@
+package dev.msnascimento.strategy.challenge;
+
+public class Customer {
+
+  public void makeBankAccountPayment(int amount) {
+    System.out.println("Payment of $" + amount + " made from bank account.");
+  }
+
+  public void makePayPalPayment(int amount) {
+    System.out.println("Payment of $" + amount + " made from PayPal.");
+  }
+
+
+}
---
+++ Behavioral/src/dev/msnascimento/strategy/challenge/Main.java
@@ -0,0 +1,10 @@
+package dev.msnascimento.strategy.challenge;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Customer customer = new Customer();
+    customer.makeBankAccountPayment(100);
+  }
+
+}
