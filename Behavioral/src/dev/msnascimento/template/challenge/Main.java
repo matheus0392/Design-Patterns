21ebd1c7acff48b4c2911c17d0d75cebeb9523d0
Matheus Nascimento <mnascimento0392@gmail.com>
2021-01-23 15:30:44 -0300

    template method pattern challenge initial

---
+++ Behavioral/src/dev/msnascimento/template/challenge/Main.java
@@ -0,0 +1,13 @@
+package dev.msnascimento.template.challenge;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Pirate pirate = new Pirate();
+    pirate.defendAgainstAttack();
+
+    Troll troll = new Troll();
+    troll.defendAgainstAttack();
+  }
+
+}
---
+++ Behavioral/src/dev/msnascimento/template/challenge/Troll.java
@@ -0,0 +1,24 @@
+package dev.msnascimento.template.challenge;
+
+public class Troll {
+
+  public void pickUpWeapon() {
+    System.out.println("Pick up club");
+  }
+
+  public void defenseAction() {
+    System.out.println("Defend with club");
+  }
+
+  public void moveToSafety() {
+    System.out.println("Return to the mountain");
+  }
+
+  public void defendAgainstAttack() {
+    pickUpWeapon();
+    defenseAction();
+    moveToSafety();
+    System.out.println();
+  }
+
+}
